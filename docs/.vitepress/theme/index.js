aa3bfa88649279b080fc3e0a799098765ddf7006
alkaid <alkaid.yang@qq.com>
2023-10-21 13:09:12 +0800

    ✨ feat(返回顶部按钮): 新增插件 vitepress-plugin-back-to-top

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,5 +1,15 @@
 import DefaultTheme from "vitepress/theme";
+// 导入返回顶部插件
+import vitepressBackToTop from 'vitepress-plugin-back-to-top'
+import 'vitepress-plugin-back-to-top/dist/style.css'
 import "./custom.css";
 export default {
   ...DefaultTheme,
+  // 返回顶部插件设置
+  enhanceApp({ app }) {
+    vitepressBackToTop({
+      // default
+      threshold:300
+    })
+  },
 };
